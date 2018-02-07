ba7f63989e4df7d44827ce8d355437033b021288
corey clippinger <coreyclip@gmail.com>
2018-02-06 17:56:20 -0800

    I made a commit, look me I'm cool kid :)

---
+++ PyPoll/main.py
@@ -0,0 +1,23 @@
+'''
+In this challenge, you are tasked with helping a small, rural town modernize its vote-counting process. (Up until now, Uncle Cleetus had been trustfully tallying them one-by-one, but unfortunately, his concentration isn't what it used to be.)
+You will be given two sets of poll data (election_data_1.csv and election_data_2.csv). Each dataset is composed of three columns: Voter ID, County, and Candidate. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+
+	* [ ]The total number of votes cast
+	* [ ]A complete list of candidates who received votes
+	* [ ]The percentage of votes each candidate won
+	* [ ]The total number of votes each candidate won
+	* [ ]The winner of the election based on popular vote.
+
+As an example, your analysis should look similar to the one below:
+Election Results
+-------------------------
+Total Votes: 620100
+-------------------------
+Rogers: 36.0% (223236)
+Gomez: 54.0% (334854)
+Brentwood: 4.0% (24804)
+Higgins: 6.0% (37206)
+-------------------------
+Winner: Gomez
+-------------------------
+'''
