1912bccf1736fe0986762db85cad640e18d38eed
corey clippinger <coreyclip@gmail.com>
2018-02-08 15:19:46 -0800

    Instead of trying to concatenate the two dicts from the two csv files, perform operation where we loop through a list of file paths and use incrimentors to tally up votes for each candidate

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,9 +1,11 @@
 """
 In this challenge, you are tasked with helping a small, rural town modernize its vote-counting process. (Up until now, Uncle Cleetus had been trustfully tallying them one-by-one, but unfortunately, his concentration isn't what it used to be.)
-You will be given two sets of poll data (election_data_1.csv and election_data_2.csv). Each dataset is composed of three columns: Voter ID, County, and Candidate. Your task is to create a Python script that analyzes the votes and calculates each of the following:
+You will be given two sets of poll data (election_data_1.csv and election_data_2.csv).
+Each dataset is composed of three columns: Voter ID, County, and Candidate. 
+Your task is to create a Python script that analyzes the votes and calculates each of the following:
 
     * [x]The total number of votes cast
-    * [ ]A complete list of candidates who received votes
+    * [x]A complete list of candidates who received votes
     * [ ]The percentage of votes each candidate won
     * [ ]The total number of votes each candidate won
     * [ ]The winner of the election based on popular vote.
@@ -27,16 +29,19 @@ import csv
 
 path1 = os.path.join('datafiles', 'election_data_1.csv')
 path2 = os.path.join('datafiles', 'election_data_2.csv')
+path_list = [path1, path2]
 
-vote_dict = {}
-
-def polltally(filepath):
-    """[summary]
+def polltally(filepath, index_start=0):
+    """[loops through a csv file of poll data and places the records
+    into a python dict while also tabulating the number of records and a 
+    list of candidates who received votes]
     
     Arguments:
         filepath {[string]} -- [filepath of csv file that contains the poll data]
+        index_start {[integer]} -- [integer to start index for vote dict]
     """
-    indexer = 0
+    vote_dict = {}
+    indexer = index_start
     vote_count = 0
     candidate_list = set()
     with open(filepath) as file:
@@ -51,25 +56,67 @@ def polltally(filepath):
             indexer += 1
             vote_count += 1
             candidate_list.add(row['Candidate'])
-            while head < 10:
+            while head <= 3:
                 print(row)
                 head += 1
             else:
                 pass
         
-    return vote_count, candidate_list
-            
+    return vote_count, candidate_list, vote_dict
 
-c1, candlist = polltally(path1)
-c2, candlist2 = polltally(path2)
+
+def votetally(candidate_list, vote_dict):
+    """[Calculates the total votes for each candidate and prints it out]
+    
+    Arguments:
+            cadidate_list {[list-like]} -- [list of candidates who's votes are being tabulated]
+            vote_dict {[dictionary of voting records]}
+    Returns:
+        tally_dict {[Dictionary]} -- [dict containing {'candidate': votes, ..}]
+    """
+    tally_dict = {}
+    for i in candidate_list:
+        vote_count = 0
+        for j in vote_dict:
+            if vote_dict[j]['vote'] == i:
+                vote_count += 1
+            else:
+                pass
+        tally_dict[i] = vote_count
+    return tally_dict
+   
+
+     
+vote_count = 0
+candidate_list = set()
+final_tally = {}             
+for path in path_list:
+    count, candlist, voterecord = polltally(path)
+    vote_count += count
+    candidate_list.update(candlist)
+    for candidate in candidate_list:
+        #loop through candidate list and call votetally to sum up how many votes
+        # each candidate got in each dataset and sum the two totals. 
+    
+                          
+
+c1, candlist, voterec1 = polltally(path1)
+c2, candlist2,voterec2 = polltally(path2)
 
 # update candidate list with the candidates from the other dataset
 candlist.update(candlist2)
 
+#tabulate the total votes from both datasets
 total_votes = c1 + c2
+
+
+        
+
+print("=" * 10 + "Election Results" + "=" * 10)
 print("total votes: " + str(total_votes))
 print()
 print("candidates who received votes: ")
+print("#" * 50)
 for i in candlist:
     print(i)
 
